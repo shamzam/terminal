5796b3da96cfd5cb4fba9e3ba24e0358f37b6fe7
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 22:01:19 +0900

    de

--- pull/views.py
+++ pull/views.py
@@ -6,9 +6,9 @@ def index(request):
     result = []
     try:
         output = subprocess.run(["cd /var/www/mymanager/mymanager/ && git reset --hard HEAD && sudo git pull && touch wsgi.py"], shell=True, capture_output=True)
-        result.append((output.stdout.decode('utf8') + '\n').replace('\n', '<br/>'))
+        result.append((output.stdout.decode('utf8') + '\n'))
         if hasattr(output, 'stderr'):
-            result.append(('stderr: ' + output.stderr.decode('utf8') + '\n').replace('\n', '<br/>'))
+            result.append(('stderr: ' + output.stderr.decode('utf8') + '\n'))
     except:
         result.append('failed')
 
