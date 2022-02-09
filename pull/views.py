b2d026d2a35b5e64b29274e8ec3e72237bec7809
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 19:09:08 +0900

    de

--- pull/views.py
+++ pull/views.py
@@ -5,7 +5,7 @@ def index(request):
     try:
         result = subprocess.run(["aa"], shell=True, capture_output=True).stdout
     except subprocess.CalledProcessError as e:
-        result = ""
+        result = "aa"
         if hasattr(e, 'output'):
             result += result + 'output: ' + e.output.decode('utf8') + "\n"
         if hasattr(e, 'stderr'):
