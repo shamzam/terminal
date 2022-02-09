a9f58a5f702ab65bbfb218b2f24f9798ea989dec
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 19:16:02 +0900

    de

--- pull/views.py
+++ pull/views.py
@@ -4,9 +4,12 @@ import subprocess
 def index(request):
     #try:
     try:
-        result = subprocess.run(["sudo"], shell=True, capture_output=True).stdout
+        result = subprocess.run(["sudo"], shell=True, capture_output=True)
+        text = result.stderr.decode('utf8') + "\n"
+        if hasattr(result, 'stderr')
+            text += text + 'stderr: ' + result.stderr.decode('utf8') + "\n"
     except:
-        result = "aa"
+        text = "aa"
     #except subprocess.CalledProcessError as e:
     #    result = "aa"
     #    if hasattr(e, 'output'):
@@ -16,4 +19,4 @@ def index(request):
     #    if hasattr(e, 'stdout'):
     #        result += result + 'stderr: ' + e.stdout.decode('utf8') + "\n"
 
-    return HttpResponse(result)
+    return HttpResponse(text)
