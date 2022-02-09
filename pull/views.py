2f4b6b03cd6dfb2ec845faf034b8f67a651e4eef
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 19:10:02 +0900

    de

--- pull/views.py
+++ pull/views.py
@@ -2,15 +2,15 @@ from django.http import HttpResponse
 import subprocess
 
 def index(request):
-    try:
-        result = subprocess.run(["aa"], shell=True, capture_output=True).stdout
-    except subprocess.CalledProcessError as e:
-        result = "aa"
-        if hasattr(e, 'output'):
-            result += result + 'output: ' + e.output.decode('utf8') + "\n"
-        if hasattr(e, 'stderr'):
-            result += result + 'stderr: ' + e.stderr.decode('utf8') + "\n"
-        if hasattr(e, 'stdout'):
-            result += result + 'stderr: ' + e.stdout.decode('utf8') + "\n"
+    #try:
+    result = subprocess.run(["aa"], shell=True, capture_output=True).stdout
+    #except subprocess.CalledProcessError as e:
+    #    result = "aa"
+    #    if hasattr(e, 'output'):
+    #        result += result + 'output: ' + e.output.decode('utf8') + "\n"
+    #    if hasattr(e, 'stderr'):
+    #        result += result + 'stderr: ' + e.stderr.decode('utf8') + "\n"
+    #    if hasattr(e, 'stdout'):
+    #        result += result + 'stderr: ' + e.stdout.decode('utf8') + "\n"
 
     return HttpResponse(result)
