c45562062f2aff480793e274af217035e7c07c7d
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 18:52:21 +0900

    added test command

--- pull/views.py
+++ pull/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+import subprocess
 
 def index(request):
-    return HttpResponse("pull page")
+    try:
+        result = subprocess.run(["whoami"], shell=true, capture_output=true).stdout
+    except CalledProcessError As e:
+        result = ""
+        if hasattr(e, 'output'):
+            result += result + 'output: ' + e.output.decode('utf8') + "\n"
+        if hasattr(e, 'stderr'):
+            result += result + 'stderr: ' + e.stderr.decode('utf8') + "\n"
+        if hasattr(e, 'stdout'):
+            result += result + 'stderr: ' + e.stdout.decode('utf8') + "\n"
+
+    return HttpResponse(result)
